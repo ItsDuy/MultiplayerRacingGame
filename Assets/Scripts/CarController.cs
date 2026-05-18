c5a8931b3a519af6a325ee91ddf726adcc8111d5
Nguyen Anh Duy <duy.nguyen280805@hcmut.edu.vn>
2026-05-18 14:24:51 +0700

    Set up Virtual Camera + Car Model + Car Controller Input System

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+namespace Kart
+{
+    public class CarController : MonoBehaviour
+    {
+        private PlayerInput playerInput; // WASD and Hold Shift for Boost, Space to Drift 
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
